23b4517ab37da65e08f016770ceddc0cadc62202
nacho_morales <ignacio@diproach.com>
2022-10-16 22:38:53 -0300

    ajuste parámetro de búsqueda

--- api/modules/clients/route.js
+++ api/modules/clients/route.js
@@ -12,12 +12,12 @@ module.exports = (module) => {
    */
   module.router.get('/search', (req, res, next) => {
 
-    if (!req.query._search) return next({ message: "Falta el parámetro '_search'" });
+    if (!req.query.q) return next({ message: "Falta el parámetro 'q'" });
 
     req.filters = {
       $or: [
-        { name: { $regex: req.query._search, $options: 'i' } },
-        { state: { $regex: req.query._search, $options: 'i' } }
+        { name: { $regex: req.query.q, $options: 'i' } },
+        { state: { $regex: req.query.q, $options: 'i' } }
       ]
     }
 
